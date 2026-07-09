f168ade04370599bf64d8ff0b094d82c2354b7bc
Vijay-kumar10 <kumar888985@gmail.com>
2026-07-09 11:09:22 +0530

    operators complete

---
+++ script.js
@@ -0,0 +1,27 @@
+// var num= 10;
+// var name  = "vijay";
+// console.log(name);
+// console.log(num);
+// let name1 = "vijay";
+// console.log(name1);
+
+// price  = 99;
+// console.log(price);
+
+name = 10;
+console.log(name);
+price = 100.0;
+console.log(typeof name);
+console.log(typeof price);
+
+const student = {
+    name : "Vijay",
+    age : 22,
+    cgpa : 7.5
+};
+
+console.log(student["name"]);
+console.log(student.name);
+console.log(student.age);
+student.age = student.age + 2;
+console.log(student.age);
